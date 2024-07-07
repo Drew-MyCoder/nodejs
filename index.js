0d83c3fb01f3a562fe8d07b768cdda60e3636c4b
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2024-07-07 04:52:04 +0000

    new routes for access and refresj token

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const errorHandler = require('./middleware/errorHandler');
 const cors = require('cors');
 const PORT = process.env.PORT || 3500;
 const corsOptions = require('./config/corsOptions');
+const verifyJWT = require('./middleware/verifyJWT');
+const cookieParser = require('cookie-parser'); 
 
 // custom middleware
 app.use(logger);
@@ -21,6 +23,10 @@ app.use(express.urlencoded({ extended: false }));
 // built-in middleware for json
 app.use(express.json());
 
+
+// middleware for cookies
+app.use(cookieParser());
+
 // serve static files
 app.use('/', express.static(path.join(__dirname, '/public')));
 
@@ -29,6 +35,9 @@ app.use('/', express.static(path.join(__dirname, '/public')));
 app.use('/', require('./routes/root'));
 app.use('/register', require('./routes/register'));
 app.use('/auth', require('./routes/auth'));
+app.use('/refresh', require('./routes/refresh'));
+
+app.use(verifyJWT);
 app.use('/employees', require('./routes/api/employees'));
 
 

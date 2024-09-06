a978ab64e8b403c37bcabdcec97cc417b87f817d
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2024-09-06 02:04:05 +0000

    for logging user out

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -1,12 +1,13 @@
-const usersDB = {
-    users: require('../model/users.json'),
-    setUsers: function (data) { this.users = data }
-}
+// const usersDB = {
+//     users: require('../model/users.json'),
+//     setUsers: function (data) { this.users = data }
+// }
+const User = require('../model/User');
 
 // const { json } = require('express');
 
-const fsPromises = require('fs').promises;
-const path = require('path');
+// const fsPromises = require('fs').promises;
+// const path = require('path');
 
 const handleLogout = async (req, res) => {
     // on client, also delete the accessToken
@@ -16,21 +17,25 @@ const handleLogout = async (req, res) => {
     const refreshToken = cookies.jwt;
 
     // is refreshtoken in db?
-    const foundUser = usersDB.users.find(person => person.refreshToken === refreshToken);
+    // const foundUser = usersDB.users.find(person => person.refreshToken === refreshToken);
+    const foundUser = await User.findOne({ refreshToken }).exec();
     if (!foundUser) {
         res.clearCookie('jwt', { httpOnly: true });
         return res.sendStatus(204);
     }
     
     // delete refresh token in db
-    const otherUsers = usersDB.users.filter(person => person.refreshToken !== foundUser.refreshToken);
-    const currentUser = { ...foundUser, refreshToken: ''};
-    usersDB.setUsers([...otherUsers, currentUser]);
-    await fsPromises.writeFile(
-        path.join(__dirname, '..', 'model', 'users.json'),
-        JSON.stringify(usersDB.users)
-    );
-
+    // const otherUsers = usersDB.users.filter(person => person.refreshToken !== foundUser.refreshToken);
+    // const currentUser = { ...foundUser, refreshToken: ''};
+    // usersDB.setUsers([...otherUsers, currentUser]);
+    // await fsPromises.writeFile(
+    //     path.join(__dirname, '..', 'model', 'users.json'),
+    //     JSON.stringify(usersDB.users)
+    // );
+    foundUser.refreshToken = '';
+    const result = await foundUser.save();
+    console.log(result);
+    
     res.clearCookie('jwt', { httpOnly: true, 
         sameSite: 'None',
         secure: true

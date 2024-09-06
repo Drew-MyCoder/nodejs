1695384ce26293cc8fdc1d5baba2ab714dc13c50
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2024-09-06 02:04:16 +0000

    refresh users token

--- controllers/refreshTokenController.js
+++ controllers/refreshTokenController.js
@@ -1,20 +1,22 @@
-const usersDB = {
-    users: require('../model/users.json'),
-    setUsers: function (data) { this.users = data }
-}
+// const usersDB = {
+//     users: require('../model/users.json'),
+//     setUsers: function (data) { this.users = data }
+// }
+const User = require('../model/User');
 
 // const { json } = require('express');
 
 const jwt = require('jsonwebtoken');
-require('dotenv').config();
+// require('dotenv').config();
 
-const handleRefreshToken = (req, res) => {
+const handleRefreshToken = async (req, res) => {
     const cookies = req.cookies;
     if (!cookies?.jwt) return res.sendStatus(401);
     console.log(cookies.jwt);
     const refreshToken = cookies.jwt;
 
-    const foundUser = usersDB.users.find(person => person.refreshToken === refreshToken);
+    // const foundUser = usersDB.users.find(person => person.refreshToken === refreshToken);
+    const foundUser = await User.findOne({ refreshToken }).exec();
     if (!foundUser) return res.sendStatus(403) // forbidden
     // evaluate jwt
 
@@ -23,7 +25,7 @@ const handleRefreshToken = (req, res) => {
         process.env.REFRESH_TOKEN_SECRET,
         (err, decoded) => {
             if (err || foundUser.username !== decoded.username) return res.sendStatus(403);
-            const roles = object.values(foundUser.roles);
+            const roles = Object.values(foundUser.roles);
             const accessToken = jwt.sign(
                 { 
                     "UserInfo": {
